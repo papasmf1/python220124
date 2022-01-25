6e37fc0a0a65ef91c916c418dd57962b62d3c636
papasmf1 <papasmf1@gmail.com>
2022-01-25 15:10:19 +0900

    람다 함수

--- Function2.py
+++ Function2.py
@@ -45,3 +45,26 @@ def union(*ar):
 #호출
 print(union("HAM","EGG"))
 print(union("HAM","EGG","SPAM"))
+
+#정의되지 않은 인자:필수, 옵션 
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    #user는 딕셔너리 
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL
+
+#호출
+print(userURIBuilder("credu.com","80",id="kim"))
+print(userURIBuilder("credu.com","80",id="kim",name="mike"))
+
+#람다함수(간결하게 함수를 정의하는 문법)
+g = lambda x,y:x*y 
+print(g(3,4))
+print(g(5,6))
+
+print( (lambda x:x*x)(3) )
+
+print( globals() )
+
+
