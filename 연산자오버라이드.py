d9e4ceed4e12910a23c532447588e43ad11fb41a
papasmf1 <papasmf1@gmail.com>
2022-01-26 11:27:36 +0900

    연산자 연습

--- 연산자오버라이드.py
+++ 연산자오버라이드.py
@@ -1,15 +1,17 @@
-#연산자 오버라이드 
+#연산자 오버라이드(연산자를 내가 원하는 로직으로 변경) 
 class NumBox:
 	def __init__(self, num):
 		self.Num = num
-	def __add__(self, num):
+	#일반 메서드로 구현 
+	def add(self, num):
 		self.Num += num
-	def __sub__(self, num):
+	#일반 메서드로 구현 
+	def remove(self, num):
 		self.Num -= num
 
 #인스턴스 생성
 n = NumBox(40)
-n + 100 
+n.add(50)
 print(n.Num)
-n - 110 
+n.remove(50)
 print(n.Num)
