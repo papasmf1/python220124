eef4f12aa4d01eaa0a404f194148a995d7d8c32e
papasmf1 <papasmf1@gmail.com>
2022-01-26 09:51:50 +0900

    클래스 연습5

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,18 @@
+#전역변수 
 str = "Not Class Member"
+
+#클래스 정의(꼼꼼하게 명확하게 코딩~~) 
 class GString:
     def __init__(self):
+        #인스턴스 멤버 변수 
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        #약간의 실수
+        print(self.str)
 
+#인스턴스 생성
 g = GString()
 g.set("First Message")
 g.print()
