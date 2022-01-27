587305c7b7e24dc24191ff9a077cdcf47787d64e
papasmf1 <papasmf1@gmail.com>
2022-01-27 11:44:59 +0900

    로그파일 분석

--- DemoRE.py
+++ DemoRE.py
@@ -14,4 +14,25 @@ print("---함정추가---")
 print(bool(re.search("[0-9]*th", "  35th")))
 print(bool(re.match("[0-9]*th", "  35th")))
 
+#주로 검색은 search()함수 사용
+if bool(re.search("apple", "this is apple")):
+    print("apple을 찾았어~~")
+else:
+    print("없음") 
 
+
+if bool(re.match("apple", "this is apple")):
+    print("apple을 찾았어~~")
+else:
+    print("없음") 
+
+#년도를 검색
+if bool(re.search("\d{4}", "올해는 2022년입니다.")):
+    print("년도 패턴을 찾았어~~")
+else:
+    print("없음") 
+
+if bool(re.match("\d{4}", "올해는 2022년입니다.")):
+    print("년도 패턴을 찾았어~~")
+else:
+    print("없음") 
