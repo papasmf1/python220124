bba9558ffae6d4cf1582c947b23042e923c30b3a
papasmf1 <papasmf1@gmail.com>
2022-01-27 16:39:30 +0900

    오늘의 유머 크롤링

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -8,11 +8,11 @@ import re
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 #파일로 저장
-f = open("c:\\work\\clien.txt", "wt", encoding="utf-8")
+f = open("c:\\work\\today.txt", "wt", encoding="utf-8")
 #페이지가 0부터시작~~ 1부터시작~~ (한페이지가 30개 * 10페이지 = 300개)
 for n in range(0,10):
         #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        data ='http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -23,17 +23,16 @@ for n in range(0,10):
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
 
-        # <span class="subject_fixed" data-role="list-title-text" title="아이폰 13 128GB 미드나이트">
-        # 	아이폰 13 128GB 미드나이트
-        # </span>
+        # <td class="subject">
+        #    <a href="/board/view.php">천주교 신부님 필력이 대단하시네요...</a>
         # 특정 조건이 있는 필터링: attributes(속성들)
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+        list = soup.find_all('td', attrs={'class':'subject'})
         for item in list:
                 try:
-                        #태그를 삭제하고 문자열 리턴 
-                        title = item.text
+                        item2 = item.find("a")
+                        title = item2.text 
                         #print(title) 
-                        if (re.search('아이폰', title)):
+                        if (re.search('대한민국', title)):
                                 print(title.strip())
                                 f.write(title.strip() + "\n")
                 except:
