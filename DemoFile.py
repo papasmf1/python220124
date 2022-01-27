1ea5ad39a3d4ecc5dd6928f6f19a978c13324f06
papasmf1 <papasmf1@gmail.com>
2022-01-27 10:10:29 +0900

    파일 연습 완료

--- DemoFile.py
+++ DemoFile.py
@@ -14,23 +14,29 @@ print("{0:f}".format(4/3))
 print("{0:.2f}".format(4/3))
 print("{0:,}".format(15000))
 
-#파일에 쓰기(유니코드로 읽기와쓰기) 
-f = open("c:\\work\\demo.txt", "wt", encoding="utf-8")
-f.write("첫줄\n두번째라인\n세번째\n")
-f.close() 
+#블럭으로 주석 처리: ctrl + / 
+# #파일에 쓰기(유니코드로 읽기와쓰기) 
+# f = open("c:\\work\\demo.txt", "wt", encoding="utf-8")
+# f.write("첫줄\n두번째라인\n세번째\n")
+# f.close() 
 
-#파일을 읽기
-f = open("c:\\work\\demo.txt", "rt", encoding="utf-8")
-#str변수로 한번에 읽기 
-print( f.read() ) 
-print( f.tell() )
-f.seek(0)
-print("---readlines()---")
-print( f.readlines() )
-f.seek(0)
-print("---readline()---")
-print( f.readline() )
-print( f.readline() )
+# #파일을 읽기
+# f = open("c:\\work\\demo.txt", "rt", encoding="utf-8")
+# #str변수로 한번에 읽기 
+# print( f.read() ) 
+# print( f.tell() )
+# #리셋(처음으로 돌아가~~)
+# f.seek(0)
+# print("---readlines()---")
+# print( f.readlines() )
+# f.seek(0)
+# print("---readline()---")
+# print( f.readline(), end="" )
+# print( f.readline(), end="" )
+
+#기존 파일에 첨부하는 경우
+f = open("c:\\work\\demo.txt", "a+", encoding="utf-8")
+f.write("새로운 데이터\n")
 
 f.close() 
 
